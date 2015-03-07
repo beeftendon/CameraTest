6a5338295222d282f25ea3142f76a6c2d36540dd
eugsong <eug.song@gmail.com>
2015-03-06 18:01:01 -0800

    Added turning of the detected edges into contours and drawing them.

--- CameraTest/FindBlob.cpp
+++ CameraTest/FindBlob.cpp
@@ -4,55 +4,52 @@
 #include <stdio.h>
 
 using namespace cv;
+using namespace std;
 
 /// Global variables
-
-Mat src, src_gray;
-Mat dst, detected_edges;
-
-int edgeThresh = 1;
-int lowThreshold=10;
-int const max_lowThreshold = 1000;
-int ratio = 3;
-int kernel_size = 3;
-char* window_name = "Edge Map";
 
 /**
 * @function CannyThreshold
 * @brief Trackbar callback - Canny thresholds input with a ratio 1:3
 */
+/*
 void CannyThreshold(int, void*)
 {
-	/// Reduce noise with a kernel 3x3
-	blur(src_gray, detected_edges, Size(3, 3));
-
-	/// Canny detector
-	Canny(detected_edges, detected_edges, lowThreshold, lowThreshold*ratio, kernel_size);
-
-	/// Using Canny's output as a mask, we display our result
-	dst = Scalar::all(0);
-
-	src.copyTo(dst, detected_edges);
-	imshow(window_name, dst);
-}
+	
+}*/
 
 
 /** @function main */
 int main(int argc, char** argv)
 {
-	/// Load an image
-	src = imread(argv[1]);
+	Mat src_img, src_ref, src_gray;
+	Mat dst, detected_edges;
 
-	if (!src.data)
+	int edgeThresh = 1;
+	int lowThreshold = 10;
+	int const max_lowThreshold = 1000;
+	int ratio = 3;
+	int kernel_size = 3;
+	char* window_name = "Edge Map";
+
+	vector<vector<Point>> contours;
+	vector<Vec4i> hierarchy;
+	RNG rng(12345);
+
+	/// Load an image
+	src_img = imread(argv[1]);
+	//src_ref = imread(argv[2]);
+
+	if (!src_img.data)
 	{
 		return -1;
 	}
 
-	/// Create a matrix of the same type and size as src (for dst)
-	dst.create(src.size(), src.type());
+	/// Create a matrix of the same type and size as src_img (for dst)
+	dst.create(src_img.size(), src_img.type());
 
 	/// Convert the image to grayscale
-	cvtColor(src, src_gray, CV_BGR2GRAY);
+	cvtColor(src_img, src_img, CV_BGR2GRAY);
 
 	/// Create a window
 	namedWindow(window_name, CV_WINDOW_AUTOSIZE);
@@ -61,7 +58,26 @@ int main(int argc, char** argv)
 	//createTrackbar("Min Threshold:", window_name, &lowThreshold, max_lowThreshold, CannyThreshold);
 
 	/// Show the image
-	CannyThreshold(0, 0);
+	//CannyThreshold(0, 0);
+
+	blur(src_img, detected_edges, Size(3, 3));
+	/// Canny detector
+	Canny(detected_edges, detected_edges, lowThreshold, lowThreshold*ratio, kernel_size);
+	findContours(detected_edges, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_NONE, Point(0, 0));
+
+	/// Draw contours
+	Mat drawing = Mat::zeros(detected_edges.size(), CV_8UC3);
+	for (int i = 0; i< contours.size(); i++)
+	{
+		Scalar color = Scalar(255, 255, 255);
+		drawContours(drawing, contours, i, color, 1, 8, hierarchy, 0, Point());
+	}
+
+	/// Using Canny's output as a mask, we display our result
+	dst = Scalar::all(0);
+
+	src_img.copyTo(dst, detected_edges);
+	imshow(window_name, drawing);
 
 	/// Wait until user exit program by pressing a key
 	waitKey(0);
