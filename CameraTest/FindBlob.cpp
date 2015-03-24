ee9206127a9107d9c83d7e30028cdeccee0d4200
eugsong <eug.song@gmail.com>
2015-03-24 16:36:14 -0700

    Does some tracking but it's really slow and inaccurate

--- CameraTest/FindBlob.cpp
+++ CameraTest/FindBlob.cpp
@@ -1,86 +1,116 @@
-#include "opencv2/imgproc/imgproc.hpp"
-#include "opencv2/highgui/highgui.hpp"
-#include <stdlib.h>
 #include <stdio.h>
+#include <iostream>
+#include "opencv2/core/core.hpp"
+#include "opencv2/features2d/features2d.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/calib3d/calib3d.hpp"
+#include "opencv2/nonfree/nonfree.hpp"
 
 using namespace cv;
-using namespace std;
 
-/// Global variables
-
-/**
-* @function CannyThreshold
-* @brief Trackbar callback - Canny thresholds input with a ratio 1:3
-*/
-/*
-void CannyThreshold(int, void*)
-{
-	
-}*/
-
+void readme();
 
 /** @function main */
 int main(int argc, char** argv)
 {
-	Mat src_img, src_ref, src_gray;
-	Mat dst, detected_edges;
 
-	int edgeThresh = 1;
-	int lowThreshold = 10;
-	int const max_lowThreshold = 1000;
-	int ratio = 3;
-	int kernel_size = 3;
-	char* window_name = "Edge Map";
+	Mat img_object = imread(argv[1], CV_LOAD_IMAGE_GRAYSCALE);
+	//Mat img_scene = imread(argv[2], CV_LOAD_IMAGE_GRAYSCALE);
+	Mat img_scene;
+	VideoCapture capture(CV_CAP_ANY);
 
-	vector<vector<Point>> contours;
-	vector<Vec4i> hierarchy;
-	RNG rng(12345);
-
-	/// Load an image
-	src_img = imread(argv[1]);
-	//src_ref = imread(argv[2]);
-
-	if (!src_img.data)
+	while (true)
 	{
-		return -1;
+		capture >> img_scene;
+		if (!img_object.data || !img_scene.data)
+		{
+			std::cout << " --(!) Error reading images " << std::endl; return -1;
+		}
+
+		//-- Step 1: Detect the keypoints using SURF Detector
+		int minHessian = 400;
+
+		SurfFeatureDetector detector(minHessian);
+
+		std::vector<KeyPoint> keypoints_object, keypoints_scene;
+
+		detector.detect(img_object, keypoints_object);
+		detector.detect(img_scene, keypoints_scene);
+
+		//-- Step 2: Calculate descriptors (feature vectors)
+		SurfDescriptorExtractor extractor;
+
+		Mat descriptors_object, descriptors_scene;
+
+		extractor.compute(img_object, keypoints_object, descriptors_object);
+		extractor.compute(img_scene, keypoints_scene, descriptors_scene);
+
+		//-- Step 3: Matching descriptor vectors using FLANN matcher
+		FlannBasedMatcher matcher;
+		std::vector< DMatch > matches;
+		matcher.match(descriptors_object, descriptors_scene, matches);
+
+		double max_dist = 0; double min_dist = 100;
+
+		//-- Quick calculation of max and min distances between keypoints
+		for (int i = 0; i < descriptors_object.rows; i++)
+		{
+			double dist = matches[i].distance;
+			if (dist < min_dist) min_dist = dist;
+			if (dist > max_dist) max_dist = dist;
+		}
+
+		printf("-- Max dist : %f \n", max_dist);
+		printf("-- Min dist : %f \n", min_dist);
+
+		//-- Draw only "good" matches (i.e. whose distance is less than 3*min_dist )
+		std::vector< DMatch > good_matches;
+
+		for (int i = 0; i < descriptors_object.rows; i++)
+		{
+			if (matches[i].distance < 3 * min_dist)
+			{
+				good_matches.push_back(matches[i]);
+			}
+		}
+
+		Mat img_matches;
+		drawMatches(img_object, keypoints_object, img_scene, keypoints_scene,
+			good_matches, img_matches, Scalar::all(-1), Scalar::all(-1),
+			vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS);
+
+		//-- Localize the object
+		std::vector<Point2f> obj;
+		std::vector<Point2f> scene;
+
+		for (int i = 0; i < good_matches.size(); i++)
+		{
+			//-- Get the keypoints from the good matches
+			obj.push_back(keypoints_object[good_matches[i].queryIdx].pt);
+			scene.push_back(keypoints_scene[good_matches[i].trainIdx].pt);
+		}
+
+		Mat H = findHomography(obj, scene, CV_RANSAC);
+
+		//-- Get the corners from the image_1 ( the object to be "detected" )
+		std::vector<Point2f> obj_corners(4);
+		obj_corners[0] = cvPoint(0, 0); obj_corners[1] = cvPoint(img_object.cols, 0);
+		obj_corners[2] = cvPoint(img_object.cols, img_object.rows); obj_corners[3] = cvPoint(0, img_object.rows);
+		std::vector<Point2f> scene_corners(4);
+
+		perspectiveTransform(obj_corners, scene_corners, H);
+
+		//-- Draw lines between the corners (the mapped object in the scene - image_2 )
+		line(img_matches, scene_corners[0] + Point2f(img_object.cols, 0), scene_corners[1] + Point2f(img_object.cols, 0), Scalar(0, 255, 0), 4);
+		line(img_matches, scene_corners[1] + Point2f(img_object.cols, 0), scene_corners[2] + Point2f(img_object.cols, 0), Scalar(0, 255, 0), 4);
+		line(img_matches, scene_corners[2] + Point2f(img_object.cols, 0), scene_corners[3] + Point2f(img_object.cols, 0), Scalar(0, 255, 0), 4);
+		line(img_matches, scene_corners[3] + Point2f(img_object.cols, 0), scene_corners[0] + Point2f(img_object.cols, 0), Scalar(0, 255, 0), 4);
+
+		//-- Show detected matches
+		imshow("Good Matches & Object detection", img_matches);
+
+		char c = waitKey(1);
+		if (c == 27) return 0;
 	}
 
-	/// Create a matrix of the same type and size as src_img (for dst)
-	dst.create(src_img.size(), src_img.type());
-
-	/// Convert the image to grayscale
-	cvtColor(src_img, src_img, CV_BGR2GRAY);
-
-	/// Create a window
-	namedWindow(window_name, CV_WINDOW_AUTOSIZE);
-
-	/// Create a Trackbar for user to enter threshold
-	//createTrackbar("Min Threshold:", window_name, &lowThreshold, max_lowThreshold, CannyThreshold);
-
-	/// Show the image
-	//CannyThreshold(0, 0);
-
-	blur(src_img, detected_edges, Size(3, 3));
-	/// Canny detector
-	Canny(detected_edges, detected_edges, lowThreshold, lowThreshold*ratio, kernel_size);
-	findContours(detected_edges, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_NONE, Point(0, 0));
-
-	/// Draw contours
-	Mat drawing = Mat::zeros(detected_edges.size(), CV_8UC3);
-	for (int i = 0; i< contours.size(); i++)
-	{
-		Scalar color = Scalar(255, 255, 255);
-		drawContours(drawing, contours, i, color, 1, 8, hierarchy, 0, Point());
-	}
-
-	/// Using Canny's output as a mask, we display our result
-	dst = Scalar::all(0);
-
-	src_img.copyTo(dst, detected_edges);
-	imshow(window_name, drawing);
-
-	/// Wait until user exit program by pressing a key
-	waitKey(0);
-
-	return 0;
 }
