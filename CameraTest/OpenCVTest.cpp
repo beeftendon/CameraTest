ce89d4d8dfb9faef7fd4872aff335ed3c20cdd37
eugsong <eug.song@gmail.com>
2015-06-08 00:42:13 -0700

    Finds largest contour (which would be the fly) and then draws bounding box around it. Bounding box can give axis of motion (still need way to find direction...either motion or centroid/moment).

--- CameraTest/OpenCVTest.cpp
+++ CameraTest/OpenCVTest.cpp
@@ -41,16 +41,19 @@ int main(int argc, char** argv)
 	int thresholdRatio = 10;
 	int kernelSize = 3;
 
-	vector<vector<Point>> contours;
+	vector<vector<Point>> contours, template_contours;
 	vector<Vec4i> hierarchy;
 	
 
 	VideoCapture capture(CV_CAP_ANY);
 
-	capture.set(CV_CAP_PROP_FPS, 480);
+	capture.set(CV_CAP_PROP_FPS, 240);
 	capture.set(CV_CAP_PROP_FRAME_HEIGHT, 200);
 	capture.set(CV_CAP_PROP_FRAME_WIDTH, 200);
-	
+
+	Mat img_template = imread(argv[1], CV_LOAD_IMAGE_GRAYSCALE);
+	//Mat img_template = imread("fly_template.jpg", CV_LOAD_IMAGE_GRAYSCALE);
+	Mat template_edges;
 	if (!capture.isOpened())
 	{
 		fprintf(stderr, "ERROR: No camera detected\n");
@@ -58,14 +61,31 @@ int main(int argc, char** argv)
 		return -1;
 	}
 
+	if (img_template.empty())
+	{
+		fprintf(stderr, "ERROR: Template image is empty");
+		getchar();
+		return -1;
+	}
+	blur(img_template, img_template, Size(10,10));
+	threshold(img_template, img_template, 10, 255, CV_THRESH_BINARY);
+
+
+	Canny(img_template, template_edges, lowThreshold, lowThreshold*thresholdRatio, kernelSize);
+	findContours(template_edges, template_contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_NONE, Point(0, 0));
+
 	//namedWindow("Camera", CV_WINDOW_AUTOSIZE);
 	namedWindow("Contours", CV_WINDOW_AUTOSIZE);
+	//namedWindow("Template", CV_WINDOW_AUTOSIZE);
 	
 	ofstream timefile;
 	timefile.open("timedata.txt");
 
+	ofstream matchfile;
+	matchfile.open("matchdata.txt");
+
 	StartCounter();
-	bool show_feed = false;
+	bool show_feed = true;
 	int total_frames = 1000;
 	while (total_frames > 0)
 	{
@@ -85,24 +105,83 @@ int main(int argc, char** argv)
 		// Convert the frame to grayscale
 		cvtColor(frame, frame, CV_BGR2GRAY);
 
+		blur(frame, frame, Size(10, 10));
+
+		threshold(frame, frame, 128, 255, CV_THRESH_BINARY);
 		//imshow("Camera", frame);
 
 		// Detect edges and find contours
 		Canny(frame, frameEdges, lowThreshold, lowThreshold*thresholdRatio, kernelSize);
+		contours.clear();
 		findContours(frameEdges, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_NONE, Point(0, 0));
+
+		// Find largest respective contours
+		int largest_area = 0;
+		int largest_contour_index_frame = -1;
+		int largest_contour_index_template = -1;
+		double area = 0;
+		vector<Point> largest_contour;
+		// Frame
+		for (int i = 0; i < contours.size(); i++)
+		{
+			area = contourArea(contours[i], false);
+			if (area > largest_area)
+			{
+				largest_area = area;
+				largest_contour_index_frame = i;
+				largest_contour = contours[i];
+			}
+		}
+		// Template
+		for (int i = 0; i < template_contours.size(); i++)
+		{
+			double area = contourArea(template_contours[i], false);
+			if (area > largest_area)
+			{
+				largest_area = area;
+				largest_contour_index_template = i;
+			}
+		}
+
+		// Find minimum bounding rectangle and ellipse(rotated)
+		RotatedRect boundingRect;
+		RotatedRect boundingEllipse;
+		if (area > 0)
+		{
+			boundingRect = minAreaRect(Mat(largest_contour));
+			boundingEllipse = fitEllipse(Mat(largest_contour));
+		}
 
 		// Draw contours
 		Mat drawing = Mat::zeros(frameEdges.size(), CV_8UC3);
-		for (int i = 0; i< contours.size(); i++)
+		Mat template_drawing = Mat::zeros(template_edges.size(), CV_8UC3);
+
+		// Draw every contour
+		/// for (int i = 0; i< contours.size(); i++)
+		/// {
+		/// 	Scalar color = Scalar(255, 255, 255);
+		/// 	drawContours(drawing, contours, i, color, 1, 8, hierarchy, 0, Point());
+		/// }
+
+		// Draw one contour
+		if (!contours.empty())
 		{
-			Scalar color = Scalar(255, 255, 255);
-			drawContours(drawing, contours, i, color, 1, 8, hierarchy, 0, Point());
+			Scalar color_white = Scalar(255, 255, 255);
+			Scalar color_red = Scalar(255, 0, 0);
+			Scalar color_green = Scalar(0, 255, 0);
+			drawContours(drawing, contours, largest_contour_index_frame, color_white, CV_FILLED, 8, hierarchy, 0, Point());
+			ellipse(drawing, boundingEllipse, color_red, 2, 8);
+			Point2f rect_points[4];
+			boundingRect.points(rect_points);
+			for (int i = 0; i < 4; i++)
+				line(drawing, rect_points[i], rect_points[(i + 1) % 4], color_green, 1, 8);
 		}
 
-		imshow("Contours", drawing);
 
 		if (show_feed == true)
 		{
+			imshow("Contours", drawing);
+			imshow("Template", template_drawing);
 			char c = cvWaitKey(1);
 			if (c == 27) break;
 		}
